b9dc26ae973a3c8e94e9dc2abe2aed4c17908a68
kalathiya vraj <84270801+kalathiyavraj@users.noreply.github.com>
2024-03-13 01:24:09 +0530

    SatrtCall.java

---
+++ SatrtCall.java
@@ -0,0 +1,20 @@
+
+    private void initToolbar() {
+        setSupportActionBar((MaterialToolbar) findViewById(R.id.material_toolbar));
+        Tools.setSystemBarColor(this, R.color.grey_5);
+        Tools.setSystemBarLight(this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search_setting, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == 16908332) {
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), menuItem.getTitle(), 0).show();
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
