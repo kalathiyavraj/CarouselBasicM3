233367d069f8c12c7cbf88d403584ff11d327158
kalathiya vraj <84270801+kalathiyavraj@users.noreply.github.com>
2024-03-14 15:32:15 +0530

    Toolbar.java

---
+++ Toolbar.java
@@ -0,0 +1,5 @@
+   private void initToolbar() {
+        setSupportActionBar((MaterialToolbar) findViewById(R.id.material_toolbar));
+        Tools.setSystemBarColor(this, R.color.grey_5);
+        Tools.setSystemBarLight(this);
+    }
